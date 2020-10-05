9cb311aba7a875772aa7fd742c509ff467cb3f14
NuriaGalliamova <ganuriya1992@yandex.ru>
2020-10-05 19:52:00 +0300

    Main.java added

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        int expenses = 1000;
+        int percent = 3;
+        int bonus = expenses * percent / 100;
+        System.out.println(bonus);
+    }
+}
